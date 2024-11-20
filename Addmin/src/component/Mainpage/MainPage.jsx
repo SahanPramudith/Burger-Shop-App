73eb27e1548e1dd718ff2115ad89c897841062aa
sahanpramudith <pramuditsahan466@gmail.com>
2024-11-20 17:34:25 +0530

    abute page done

--- Addmin/src/component/Mainpage/MainPage.jsx
+++ Addmin/src/component/Mainpage/MainPage.jsx
@@ -9,12 +9,11 @@ export default function MainPage() {
     return (
         <div>
 
-
             <div class="grid mb-8 border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2 bg-white dark:bg-gray-800">
                 <NavLink to={'/bugermenueu'}>
                     <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
                         <figcaption class="flex items-center justify-center ">
-                            <img class="rounded-full w-full h-full" src={buger} alt="profile picture" />
+                            <img class="rounded-full w-48" src={buger} alt="profile picture" />
 
                         </figcaption>
                         <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
@@ -29,7 +28,7 @@ export default function MainPage() {
                 <NavLink to={'/pitzamenue'}>
                     <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
                         <figcaption class="flex items-center justify-center ">
-                            <img class="rounded-full w-full h-full" src={pizza} alt="profile picture" />
+                            <img class="rounded-full w-48 h-48 " src={pizza} alt="profile picture" />
 
                         </figcaption>
                         <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
@@ -42,7 +41,7 @@ export default function MainPage() {
                 <NavLink to={'/Chikenmenue'}>
                     <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
                         <figcaption class="flex items-center justify-center ">
-                            <img class="rounded-full w-full h-full" src={chicken} alt="profile picture" />
+                            <img class="rounded-full w-48 h-48" src={chicken} alt="profile picture" />
 
                         </figcaption>
                         <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
@@ -52,12 +51,48 @@ export default function MainPage() {
                     </figure>
                 </NavLink>
 
+                <button data-modal-target="popup-modal" data-modal-toggle="popup-modal"  type="button">
+                    <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
+                        <figcaption class="flex items-center justify-center ">
+                            <img class="rounded-full w-48 h-48" src={chicken} alt="profile picture" />
+
+                        </figcaption>
+                        <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+                            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Very easy this was to integrate</h3>
+                            <p class="my-4">If you care for your time, I hands down would go with this."</p>
+                        </blockquote>
+                    </figure>
+                </button>
+
 
             </div>
-            <Routes>
 
-                <Route path='/pitzamenue' element={<PizzaView />} />
-            </Routes>
+
+
+
+
+            <div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class="relative p-4 w-full max-w-md max-h-full">
+                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
+                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                            </svg>
+                            <span class="sr-only">Close modal</span>
+                        </button>
+                        <div class="p-4 md:p-5 text-center">
+                            <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                            </svg>
+                            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">This product is coming soon</h3>
+                            
+                            <button data-modal-hide="popup-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">OK</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
 
         </div>
     )
--- front end/src/component/Aboute/Aboute.jsx
+++ front end/src/component/Aboute/Aboute.jsx
@@ -1,46 +1,57 @@
 import React from 'react';
 import './Aboute.css';
 
-const Aboute = () => {
-    return (
-        <div className="container">
-            {/* Parent container for responsiveness */}
-            <div className="row align-items-center">
-                {/* Left Column */}
-                <div className="col-12 col-md-6 mb-4 mb-md-0">
-                    <div className="d-flex flex-column justify-content-center align-items-center">
-                        <img
-                            src="your-image-1.jpg"
-                            alt="Description 1"
-                            className="img-fluid rounded-circle mb-3"
-                            style={{ maxWidth: '100%', height: 'auto', borderRadius: '140px' }}
-                        />
-                        <img
-                            src="your-image-2.jpg"
-                            alt="Description 2"
-                            className="img-fluid"
-                            style={{ marginTop: '-50px', marginLeft: '20px', maxWidth: '80%' }}
-                        />
-                    </div>
-                </div>
 
-                {/* Right Column */}
-                <div className="col-12 col-md-6 d-flex flex-column align-items-center">
-                    <img
-                        src="your-image-3.jpg"
-                        alt="Description 3"
-                        className="img-fluid"
-                        style={{
-                            borderRadius: '140px 140px 0px 140px',
-                            height: 'auto',
-                            margin: '15px',
-                        }}
-                    />
-                    <div className="opacity-50 mt-2">Some text or other content here</div>
+function Aboute() {
+    return (
+        <div>
+            {/* <MenueBanner img={'src/assets/bgimg.jpeg'} heding={"About Us"} /> */}
+            <div className="container">
+                <div className="row about-container">
+                    {/* Left Section */}
+                    <div className="col-lg-6 col-md-6 col-sm-12 about-left">
+                        <img src="src\assets\shape-2 - Copy.png" alt="" className="img-fluid" />
+                        <div className="row">
+                            <div className="col-6">
+                                <img className="Ab-img-1 img-fluid" src="src/assets/Ab-1.jpg" alt="" />
+                            <img className="Ab-img-2 img-fluid" src="src/assets/Ab-1.jpg" alt="" />
+                            
+                            </div>
+                            <div className="col-6">
+                            </div>
+                        </div>
+                        <div>
+                            <img className="Ab-img-3 img-fluid" src="src/assets/Ab-2.jpg" alt="" />
+                        </div>
+                    </div>
+
+                    {/* Right Section */}
+                    <div className="col-lg-6 col-md-6 col-sm-12 about-right">
+                        <div fontFamily={'fantasy'} className="text-center mt-4" variant="h1" gutterBottom>
+                            About Us
+                        </div>
+                        <div className="text-center mt-3" variant="h4" gutterBottom>
+                            The Cleanest, Most Delicious Vegan Cookie Dough Ever.
+                        </div>
+                        <div className="mt-4">
+                            <div variant="body1" className="txt text-center">
+                                But that you may see whence every one is born, the pleasure and pain of those who accuse him
+                                I will open the whole of the praisers, and they will be discovered by the searcher of the truth and like an arch
+                                I will explain the roof of the happy life.
+                            </div>
+                        </div>
+                        <div className="d-flex justify-content-center mt-3 bg-opacity-10">
+                            <img src="src/assets/icon-1.png" alt="" className="m-2" />
+                            <img src="src/assets/icon-2.png" alt="" className="m-2" />
+                            <img src="src/assets/icon-3.png" alt="" className="m-2" />
+                            <img src="src/assets/icon-4.png" alt="" className="m-2" />
+                            <img src="src/assets/icon-5.png" alt="" className="m-2" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     );
-};
+}
 
 export default Aboute;
